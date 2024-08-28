ee7fd0656d63214efeef9081a1c2c01aed6de6fe
Michael <blondinmichael@gmail.com>
2024-08-28 13:03:35 -0400

    Ajout max

---
+++ chapitre1/max.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+def max_iter(s):                   # Décompte du nombre d'opérations élémentaires:
+    i = 1                          ; nb_oper_elem  = 1 # affectation
+    m = s[0]                       ; nb_oper_elem += 2 # affectation + accès
+
+    pass                           ; nb_oper_elem += 1 # condition (1ère fois)
+
+    while i < len(s):
+        pass                       ; nb_oper_elem += 2 # accès + comparaison
+        if s[i] > m:
+            m = s[i]               ; nb_oper_elem += 2 # affectation + accès
+            
+        i = i + 1                  ; nb_oper_elem += 2 # affectation + addition
+        pass                       ; nb_oper_elem += 1 # condition (autre fois)
+
+    return (m, nb_oper_elem)
+
+if __name__ == "__main__":
+    # Exemple du nombre d'opérations élémentaires exécutées sur les
+    # entrées de taille 1, 2, 3 et 4
+    for n in range(0, 4):
+        entrees_taille_n = map(list, permutations(range(1, n+2)))
+
+        for s in entrees_taille_n:
+            m, nb_oper = max_iter(s)
+            
+            print("Entrée:", s, "  Sortie:", m, "  Nombre opér. élém.:", nb_oper)
+
+        print()
