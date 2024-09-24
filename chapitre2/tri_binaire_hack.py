66609c24717e3f54b7f173840a5a7e3a6b7875f0
Michael <blondinmichael@gmail.com>
2024-09-24 11:26:07 -0400

    Mineur

--- chapitre2/tri_binaire_hack.py
+++ chapitre2/tri_binaire_hack.py
@@ -2,9 +2,13 @@ def tri_binaire(s):
     i = 0
     j = len(s) - 1
 
-    while i < j:        
-        s[i], s[j] = s[j], s[i]
-        i += (1 - s[i])
+    while i < j:
+        # Échanger s[i] et s[j] avec trois OU exclusifs
+        s[i] = s[i] ^ s[j]
+        s[j] = s[i] ^ s[j]
+        s[i] = s[i] ^ s[j]
+
+        i += not s[i]
         j -= s[j]
 
     return s
